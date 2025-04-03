a2ab92ec0c8f7c8ead4180caee8a4bbdd0fb2282
sydflake <142807327+sydflake@users.noreply.github.com>
2025-04-03 15:10:34 -0500

    Update extract.py

---
+++ extract.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import requests
+
+url = 'https://data.cityofchicago.org/api/odata/v4/4ijn-s7e5'
+response = requests.get(url)
+
+data = pd.read_json(response.text)
+data.head()
+
+all_data = []
+top = 1000
+skip = 0
+
+while True:
+    paged_url = f'https://data.cityofchicago.org/api/odata/v4/4ijn-s7e5?$top={top}&$skip={skip}'
+    response = requests.get(paged_url)
+    page_data = response.json().get('value', [])
+    
+    if not page_data:
+        break
+
+    all_data.extend(page_data)
+    skip += top
+
+# Convert to DataFrame
+dataframe1 = pd.DataFrame(all_data)
+display(dataframe1.head())
