200eac05a5a091ed6156896ddcd131da769c278d
manaswiluitel <96408121+manaswiluitel@users.noreply.github.com>
2025-04-17 14:53:58 -0500

    Update transform.py

--- transform.py
+++ transform.py
@@ -1,6 +1,7 @@
 df.drop(columns = ['__id', 'dba_name', 'aka_name', 'license_', 'latitude', 'longitude', 'location', 'location_address','location_city', 'location_state', 'location_zip', 'address', ], inplace=True)
 df = df[df['state'] == 'IL']
 df.drop(columns = ['state', 'city'], inplace =True)
+df = df.dropna(subset=['zip'])
 df.drop_duplicates(inplace=True)
 
 def strip_and_lower(df, column_name):
