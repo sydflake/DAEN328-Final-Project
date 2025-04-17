e39329de38c8961bbce1d9dd2e9d98d9e1a5a23d
manaswiluitel <96408121+manaswiluitel@users.noreply.github.com>
2025-04-17 15:36:55 -0500

    Update transform.py

--- transform.py
+++ transform.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from rapidfuzz import fuzz, process
 import re
 
-df.drop(columns = ['__id', 'dba_name', 'aka_name', 'license_', 'latitude', 'longitude', 'location', 'location_address','location_city', 'location_state', 'location_zip', 'address', ], inplace=True)
+df.drop(columns = ['__id', 'aka_name', 'license_', 'latitude', 'longitude', 'location', 'location_address','location_city', 'location_state', 'location_zip', 'address', ], inplace=True)
 df = df[df['state'] == 'IL']
 df.drop(columns = ['state', 'city'], inplace =True)
 df = df.dropna(subset=['zip'])
