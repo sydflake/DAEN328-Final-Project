ae815a3976a50c7268123b050fafb2e816fde35c
manaswiluitel <96408121+manaswiluitel@users.noreply.github.com>
2025-04-17 14:34:36 -0500

    Update transform.py

---
+++ transform.py
@@ -0,0 +1,1 @@
+df.drop(columns = ['__id', 'aka_name', 'license_', 'latitude', 'longitude', 'location', 'location_address','location_city', 'location_state', 'location_zip', 'address', ], inplace=True)
