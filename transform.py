d843c456a5b21df6dc10636edcb30d3739c002ba
manaswiluitel <96408121+manaswiluitel@users.noreply.github.com>
2025-04-17 14:39:05 -0500

    Update transform.py

--- transform.py
+++ transform.py
@@ -1,1 +1,3 @@
 df.drop(columns = ['__id', 'aka_name', 'license_', 'latitude', 'longitude', 'location', 'location_address','location_city', 'location_state', 'location_zip', 'address', ], inplace=True)
+df = df[df['state'] == 'IL']
+df.drop(columns = ['state', 'city'], inplace =True)
