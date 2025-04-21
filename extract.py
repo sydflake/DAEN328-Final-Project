82f82e15ed100c707f2fbc23ab49ee0e41484bae
sydflake <142807327+sydflake@users.noreply.github.com>
2025-04-21 12:21:37 -0500

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,26 +1,30 @@
 import requests
 import pandas as pd
 
-url = 'https://data.cityofchicago.org/api/odata/v4/4ijn-s7e5'
-response = requests.get(url)
+def fetch_data_from_api():
+    url = 'https://data.cityofchicago.org/api/odata/v4/4ijn-s7e5'
+    response = requests.get(url)
 
-data = pd.read_json(response.text)
-data.head()
+    data = pd.read_json(response.text)
+    data.head()
 
-all_data = []
-top = 1000
-skip = 0
+    all_data = []
+    top = 1000
+    skip = 0
 
-while True:
-    paged_url = f'https://data.cityofchicago.org/api/odata/v4/4ijn-s7e5?$top={top}&$skip={skip}'
-    response = requests.get(paged_url)
-    page_data = response.json().get('value', [])
-    
-    if not page_data:
-        break
+    while True:
+        paged_url = f'https://data.cityofchicago.org/api/odata/v4/4ijn-s7e5?$top={top}&$skip={skip}'
+        response = requests.get(paged_url)
+        page_data = response.json().get('value', [])
+        
+        if not page_data:
+            break
 
-    all_data.extend(page_data)
-    skip += top
+        all_data.extend(page_data)
+        print('Fetched page', skip)
+        skip += top
 
-# Convert to DataFrame
-df = pd.DataFrame(all_data)
+    # Convert to DataFrame
+    df = pd.DataFrame(all_data)
+
+    return df
